266d1cd5aa731ad1acd6f5de9c90286e076eeaec
Khang Tran <khangtrann8198@gmail.com>
2026-04-21 17:15:03 +0700

    feat: integrate trpc

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,5 +1,16 @@
+"use client"
+
+import { useTRPC } from "@/trpc/client"
+import { useQuery } from "@tanstack/react-query";
+
 export default function Home() {
+  const trpc = useTRPC();
+  const categories = useQuery(trpc.categories.getMany.queryOptions());
+
   return (
-    <div>Home</div>
-  );
-};
+    <div>
+      <p>is loading: {categories.isLoading + ''}</p>
+      {JSON.stringify(categories.data)}
+    </div>
+  )
+}
