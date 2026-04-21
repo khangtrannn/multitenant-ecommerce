3691c6baa6846ff1a07b662ae4aff2492e542afe
Khang Tran <khangtrann8198@gmail.com>
2026-04-21 10:46:44 +0700

    feat: customize ui to follow neo-brutalism-like

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <p>Hello World</p>
+    <div className="p-4">
+      <Button variant="elevated">Hello World</Button>
+      <Input placeholder="Hello Input" />
     </div>
   )
 }
