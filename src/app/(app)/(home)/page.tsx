ec67ea8872a15fc881de68636a21fc557e5b3685
Khang Tran <khangtrann8198@gmail.com>
2026-04-22 10:44:20 +0700

    feat: implement authentication flow with sign-in and sign-up views, TRPC procedures, and user schemas

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -5,12 +5,11 @@ import { useQuery } from "@tanstack/react-query";
 
 export default function Home() {
   const trpc = useTRPC();
-  const categories = useQuery(trpc.categories.getMany.queryOptions());
+  const { data } = useQuery(trpc.auth.session.queryOptions());
 
   return (
     <div>
-      <p>is loading: {categories.isLoading + ''}</p>
-      {JSON.stringify(categories.data)}
+      {JSON.stringify(data?.user, null, 2)}
     </div>
   )
 }
