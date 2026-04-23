4861d7b8ad6ef92afa63ab0a93a369d3380a7f50
Khang Tran <khangtrann8198@gmail.com>
2026-04-23 18:32:32 +0700

    feat: implement access control for collections, restrict create, update, and delete actions to super-admins

--- src/collections/Tenants.ts
+++ src/collections/Tenants.ts
@@ -1,7 +1,12 @@
+import { isSuperAdmin } from '@/lib/access';
 import type { CollectionConfig } from 'payload'
 
 export const Tenants: CollectionConfig = {
   slug: 'tenants',
+  access: {
+    create: ({ req }) => isSuperAdmin(req.user),
+    delete: ({ req }) => isSuperAdmin(req.user),
+  },
   admin: {
     useAsTitle: 'slug',
   },
@@ -35,13 +40,19 @@ export const Tenants: CollectionConfig = {
       name: "stripeAccountId",
       type: "text",
       required: true,
+      access: {
+        update: ({ req }) => isSuperAdmin(req.user),
+      },
       admin: {
-        readOnly: true,
+        description: "Stripe Account ID associated with your shop",
       },
     },
     {
       name: "stripeDetailsSubmitted",
       type: "checkbox",
+      access: {
+        update: ({ req }) => isSuperAdmin(req.user),
+      },
       admin: {
         readOnly: true,
         description: "You cannot create products until you submit your Stripe details",
